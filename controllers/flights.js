b7e7e5e98c43c06608b03b120cad78420abfe7a3
DanielleColucci <ddcolucci99@gmail.com>
2023-01-09 21:17:37 -0500

    define DELETE /flights/:id route and define/export delete flight controller function

--- controllers/flights.js
+++ controllers/flights.js
@@ -30,8 +30,19 @@ function create(req, res) {
   })
 }
 
+function deleteFlight(req, res) {
+  Flight.findByIdAndDelete(req.params.id)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    res.redirect('/')
+  })
+}
+
 export {
   index, 
   newFlight as new, 
   create,
+  deleteFlight as delete
 }
