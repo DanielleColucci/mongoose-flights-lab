b0373eaa8e732a9c2416e30a8a434520a055d976
DanielleColucci <ddcolucci99@gmail.com>
2023-01-10 09:18:17 -0500

    fix default date value

--- models/flight.js
+++ models/flight.js
@@ -6,9 +6,17 @@ const flightSchema = new Schema({
   airline: String, 
   airport: {type: String, default: 'DEN'},
   flightNo: Number,
-  departs: {type: Date, default: function() {
-    return new Date().getFullYear + 1
-  }}
+  departs: {
+    type: Date, 
+    default: function() {
+      const today = new Date()
+      console.log(today)
+      const nextYear = today.getFullYear() + 1
+      console.log(nextYear)
+      today.setFullYear(nextYear)
+      return today 
+    }
+  }
 }, {
   timestamps: true
 })
