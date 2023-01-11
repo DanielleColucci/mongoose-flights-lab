15c6a52f1fae65d895b9a619cda0e1c7c39f609d
DanielleColucci <ddcolucci99@gmail.com>
2023-01-10 19:47:01 -0500

    create ticketSchema and add as embedded data in flightSchema

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema({
+  set: {type: String, match: /[A-F][1-9]\d?/},
+  price: {type: Number, min: 0}
+})
+
 const flightSchema = new Schema({
   airline: {
     type: String, 
@@ -20,7 +25,8 @@ const flightSchema = new Schema({
       today.setFullYear(nextYear)
       return today 
     }
-  }
+  }, 
+  tickets: [ticketSchema]
 }, {
   timestamps: true
 })
